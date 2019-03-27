6e4d3d3b050bb07ffc6fd07e9305e4a36b3df5d1
Bene <bk191414@gmail.com>
2019-03-27 17:15:40 +0100

    ...

--- HonoluluArt/ViewController.swift
+++ HonoluluArt/ViewController.swift
@@ -36,6 +36,7 @@ class ViewController: UIViewController {
     
     // globale Deklaration
     @IBOutlet weak var mapView: MKMapView!
+    var artworks: [Artwork] = []
     
     let regionRadius: CLLocationDistance = 1000
     
@@ -45,6 +46,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
         centerMapOnLocation(location: initialLocation)
+        
+        mapView.delegate = self
+    
+        // nur ein eine Nadel wird gesetzt
+     //   let artwork = Artwork(title: "King David Kalakaua", locationName: "Waikiki Gateway Park", discipline: "Sculpture", coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+    // mapView.addAnnotation(artwork)
+        
+        
+        loadInitialData()
+        mapView.addAnnotations(artworks)
+        
         
         
     }
@@ -59,5 +71,67 @@ class ViewController: UIViewController {
         
     }
     
+    
+    func loadInitialData() {
+        
+        
+        guard let fileName = Bundle.main.path(forResource: "PublicArt", ofType: "json") else {
+            return  }
+        let optionalData = try? Data(contentsOf: URL(fileURLWithPath: fileName))
+        
+        guard let data = optionalData,
+            let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments),
+            let dictionary = json as? [String: Any],
+            let works = dictionary["data"] as? [[Any]] else {
+                return
+        }
+        
+        let validWorks = works.flatMap { Artwork(json: $0) }
+        
+        artworks.append(contentsOf: validWorks)
+    }
+    
 }
 
+extension ViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        
+        // Typecast zu Artwork
+        guard let annotation = annotation as? Artwork else { return nil }
+        
+        
+        let identifier = "marker"
+        var view: MKMarkerAnnotationView
+        
+        if let dequeudView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView {
+            
+            dequeudView.annotation = annotation
+            view = dequeudView
+        } else {
+            
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            
+        }
+        
+        return view
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        let location = view.annotation as! Artwork
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+        
+        location.mapItem().openInMaps(launchOptions: launchOptions)
+        
+    }
+    
+    
+    
+    
+}
